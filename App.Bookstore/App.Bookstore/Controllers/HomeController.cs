cbcb0186fa9da5eb9f3e9f3d12882f0c5b499270
sathish <sanduvenkatasathish@sathish.com>
2024-02-05 12:38:44 +0530

    added mvc to application

---
+++ App.Bookstore/App.Bookstore/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "WebApplication";
+        }
+    }
+}
---
+++ BookStore.App/BookStore.App/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "BookStore Application";
+        }
+    }
+}
